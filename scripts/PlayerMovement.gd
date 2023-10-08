9fc7d783028a7a42f5d61786aa982b1c086d748f
Jakub <35482785+Zimoslaw@users.noreply.github.com>
2023-10-08 22:51:29 +0200

    jumping

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -1,11 +1,18 @@
 extends Node3D
 
-var MovementSpeed: float = 5
-var RotationSpeed: float = 360
-var isRotating: bool = false
-var rotationDirection: int = 0
+# Movement parameters
+var movementSpeed: float = 5
+const rotationSpeed: float = 360
+const jumpingSpeed: float = 10
+const jumpingLength: float = 2
 
-var prevDegrees: float
+var rotationDirection: int = 0
+var prevDegrees: float = 0
+var deltaHeight: float = 0
+
+# States
+var isRotating: bool = false
+var isJumping: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,33 +21,40 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	doJump()
+	doJump(delta)
 	doRotation(delta)
 	doMovement(delta)
 	
 func doMovement(delta: float):
 	if Input.is_action_pressed("MoveForward"):
-		translate_object_local(Vector3(0, 0, MovementSpeed * delta))
+		translate(Vector3(0, 0, movementSpeed * delta))
 		
 	if Input.is_action_pressed("MoveBackwards"):
-		translate_object_local(Vector3(0, 0, (-MovementSpeed * delta) /2))
+		translate(Vector3(0, 0, (-movementSpeed * delta) /2))
 
 func doRotation(delta: float):
-	if Input.is_action_just_pressed("MoveRight") && !isRotating:
+	if Input.is_action_just_pressed("MoveRight") && !isRotating && !isJumping:
 		isRotating = true
 		rotationDirection = -1
 		prevDegrees = rotation_degrees.y
-	elif Input.is_action_just_pressed("MoveLeft") && !isRotating:
+	elif Input.is_action_just_pressed("MoveLeft") && !isRotating && !isJumping:
 		isRotating = true
 		rotationDirection = 1
 		prevDegrees = rotation_degrees.y
 		
 	if isRotating:
-		rotation_degrees.y += rotationDirection * RotationSpeed * delta
+		rotation_degrees.y += rotationDirection * rotationSpeed * delta
 		if abs(rotation_degrees.y - prevDegrees) >= 90:
 			rotation_degrees.y = prevDegrees + (90 * rotationDirection)
 			isRotating = false
 
 func doJump(delta: float):
-	if Input.is_action_just_pressed("Jump"):
+	if Input.is_action_just_pressed("Jump") && !isJumping:
+		isJumping = true
 		
+	if isJumping:
+		deltaHeight += (jumpingSpeed * delta)
+		translate(Vector3(0, 0.2 * sin(deltaHeight), 0.314 * jumpingSpeed * delta))
+		if deltaHeight >= (jumpingLength * PI):
+			isJumping = false
+			deltaHeight = 0
